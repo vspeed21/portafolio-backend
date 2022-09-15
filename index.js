f25d6f7e73f2952193051e7bc914896331a10833
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-09-15 12:43:48 -0600

    cambio variable de entorno

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 dotenv.config()
 conectarDB();
 
-const dominiosPermitidos = ['https://portafolio-tikodev.vercel.app'];
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 const corsOption = {
   origin: function(origin, callback) {
