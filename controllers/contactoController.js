afebc5e42608385707f06538a1a2748b539067bd
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-09-29 16:49:49 -0600

    add: conditional lang message to response registrar function

--- controllers/contactoController.js
+++ controllers/contactoController.js
@@ -3,24 +3,23 @@ import Contacto from "../models/contactoModel.js";
 
 const registrar = async (req, res) => {
   const { emailOrCell } = req.body;
+  const { lang } = req.query;
 
   const existeEmail = await Contacto.findOne({ emailOrCell });
 
   if (existeEmail) {
-    const error = new Error('Duplicate message');
+    const error = new Error(lang == 'es-ES' || lang == 'es' ? 'Mensaje duplicado' : 'Duplicate message');
     return res.status(404).json({ msg: error.message });
   }
 
   try {
     const contacto = new Contacto(req.body);
     await contacto.save();
-    res.json({ msg: 'Message sent successfully' })
+    res.json({ msg: lang == 'es-ES' || lang == 'es' ? 'Mensaje enviado correctamente' : 'Message sent successfully' });
 
   } catch (error) {
     console.log(error);
   }
-
-  // res.send({msg: 'Mensaje enviado correctamente'});
 }
 
 const sendEmail = async (req, res) => {
